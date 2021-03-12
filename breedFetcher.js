da093669e271d7d9134ddb15e74b086ba04cb286
Ruowen Tang <ruowen.tang@gmail.com>
2021-03-11 21:07:09 -0500

    refactored with error handling

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,22 @@
 const request = require('request');
-const userInput = process.argv[2];
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${userInput}`;
 
-request(url, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
 
-  if (error) {
-    console.log('URL is invalid');
-  } else if (!JSON.parse(body)[0]) {
-    console.log('Cannot find breed info');
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+
+    if (error) {
+      callback('URL is invalid', null);
+
+    } else if (!JSON.parse(body)[0]) {
+  
+      callback('Cannot find breed info', null);
+    } else {
+      const data = JSON.parse(body);
+      callback(null, data[0].description);
+    }
+  });
+
+};
+
+module.exports = {fetchBreedDescription};
